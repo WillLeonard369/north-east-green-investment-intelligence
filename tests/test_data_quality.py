5880bd747c258964a4d6be82c956ddc0f01fa74e
William Finnie <willowenf4@gmail.com>
2026-06-20 17:30:34 +0100

    Add ORE Catapult Blyth testing expansion

--- tests/test_data_quality.py
+++ tests/test_data_quality.py
@@ -95,10 +95,10 @@ def test_green_projects_summary_output():
 
     metrics = dataframe.set_index("metric")["value"]
 
-    assert metrics["projects"] == 9
+    assert metrics["projects"] == 12
     assert (
         metrics["committed_regional_capital_investment_gbp"]
-        == 2_280_000_000
+        == 2_365_600_000
     )
     assert pd.isna(
         metrics["estimated_regional_capital_investment_gbp"]
@@ -109,7 +109,7 @@ def test_green_projects_summary_output():
     )
     assert metrics["regional_economic_impact_gbp"] == 262_200_000
     assert metrics["construction_jobs"] == 3_700
-    assert metrics["operational_jobs"] == 2_071
+    assert metrics["operational_jobs"] == 2_301
     assert metrics["jobs_supported"] == 13_000
-    assert metrics["regional_jobs_announced"] == 18_771
+    assert metrics["regional_jobs_announced"] == 19_001
     
